6111732a3f89842e2a51eb72d3a72e8ceb502664
cpc120 <cleocarreon097@gmail.com>
2024-06-25 18:44:14 -0400

    Add files via upload

---
+++ volunteers.js
@@ -0,0 +1,71 @@
+var $ = function (id) { return document.getElementById(id); };
+
+var volunteerArray = [];
+
+var displayVolunteers = function () {   
+    // display the volunteers in the text area
+    var volunteerListString="";
+    for (var i = 0; i < volunteerArray.length; i++){
+        volunteerListString = volunteerListString + volunteerArray[i]+ "\n";
+    }
+    $("volunteerList").value= volunteerListString;
+	
+};
+
+var addVolunteer = function () {
+    // get the data from the form
+    var volunteerString = $("first_name").value + " " + $("last_name").value;
+
+    // store the data in an array
+    volunteerArray.push(volunteerString);
+    
+    // display the volunteers and clear the add form
+    displayVolunteers();
+    
+    // get the add form ready for next entry
+    $("first_name").value = "";
+    $("last_name").value = "";
+    $("first_name").focus();
+};
+
+
+var deleteVolunteer = function () {
+    var volunteerString = $("first_name").value + " " + $("last_name").value; 
+    for (var i=0; i < volunteerArray.length; i++){
+        if (volunteerArray[i].trim == volunteerString.trim()){
+            volunteerArray.splice(i,1);
+        }
+    }
+	 
+    // display the volunteers and clear the add form
+    displayVolunteers();
+    
+    // get the delete form ready for next entry
+    $("first_name").value = "";
+    $("last_name").value = "";
+    $("first_name").focus();
+};
+
+var clearList = function () {   
+    // delete the data from the arrays
+    volunteerArray = [];
+    $("volunteerList").value = "";
+    $("first_name").focus();
+};
+
+var sortList = function () {   
+    // sort the scores
+    volunteerArray.sort();
+    
+    // display the scores
+    displayVolunteers();    
+};
+
+//When the page is fully loaded, the buttons will be mapped to the JavaScript functions
+window.onload = function () {
+    $("add_button").onclick = addVolunteer;
+	$("delete_button").onclick = deleteVolunteer;
+    $("clear_button").onclick = clearList;    
+    $("sort_button").onclick = sortList;    
+    $("first_name").focus();
+}
