3414cad33a66e976af1d0bccdc82ea73e76c1e46
cpc120 <cleocarreon097@gmail.com>
2024-06-26 20:02:28 -0400

    Add files via upload

--- volunteers.js
+++ volunteers.js
@@ -1,61 +1,140 @@
-var $ = function(id){
-  return document.getElementById(id); };
-  
-  var volunteerListArray =[];
-  var displayVolunteerList = function () {
-    
-    //display voluteers in Text are
-    var voluteerListString="";
-    for (var i=0; i < volunteerListArray.length();i++)
-  var addVolunteer=function() {
-    
-    var volunteerString = $("first_name").value() + "" + $("last_name").value();
-    volunteerListArray.push(volunteerString);
-    
-    //display the volunteers in a clear form
-    displayVolunteerList();
-    //clear fields
-    $("first_name").value() = "";
-    $("last_name").value() = "";
-    $("first_name").focus();
-  }
-  var deleteVolunteer = function(){
-    var volunteerString = $("first_name").value() + "" + $("last_name").value();
+var $ = function (id) {
+  return document.getElementById(id);
+};
 
-    //remove the string from the array
-    for (var i=0; i < volunteerListArray.length(); i++){
-      if (volunteerListArray[i].trim() == volunteerString.trim()){
-        volunteerListArray.splice(i,1);
-      }
+var volunteerArray = [];
+
+var displayVolunteers = function () {
+  // display the volunteers in the text area
+
+  var i;
+
+  $("volunteerList").value = "";
+
+  for (i = 0; i < volunteerArray.length; i++) {
+    $("volunteerList").value += i + 1 + ". " + volunteerArray[i] + "\n";
+  }
+
+
+  // comment out the line above change this to a loop instead to loop through the array.
+};
+
+var addVolunteer = function () {
+  // get the data from the form
+
+  var volunteerString = $("first_name").value + " " + $("last_name").value;
+
+  // store the data in an array
+
+  volunteerArray.push(volunteerString);
+
+  // display the volunteers and clear the add form
+
+  displayVolunteers();
+
+  // get the add form ready for next entry
+
+  $("first_name").value = "";
+
+  $("last_name").value = "";
+
+  $("first_name").focus();
+};
+
+var deleteVolunteer = function () {
+  // get the data from the form (hint: use the same format as from the add).
+
+  var volunteerString = $("first_name").value + " " + $("last_name").value;
+
+  // remove the string from the array (hint, loop through the entire list, compare the string with the item in the array.
+
+  for (var i = volunteerArray.length - 1; i >= 0; i--) {
+    if (volunteerArray[i] === volunteerString) {
+      volunteerArray.splice(i, 1);
     }
-    //display the volunteers
-    displayVolunteerList();
-
-    //delete fields 
-    $("first_name").value() = "";
-    $("last_name").value() = "";
-    $("first_name").focus();
-
-    //JS functions
-    window.onload = function () {
-      $("add_button").onclick = addVolunteer;
-      $("delete_button").onclick = deleteVolunteer;
-      $("clear_button").onclick = clearList;
-      $("first_name").focus();      
   }
-}; 
-//comparing the first names and sorting based on function
-function compare(a,b){
-  var splitA = a.split("");
-  var splitB = b.split("");
-  var first_nameA = splitA[splitA.length - 1];
-  var first_nameB = splintB[splintB.length - 1];
-  if (first_nameA < first_nameB) return -1;
-  if (first_nameA > first_nameB) return 1;
+
+  // display the volunteers and clear the add form
+
+  displayVolunteers();
+
+  // get the delete form ready for next entry
+
+  $("first_name").value = "";
+
+  $("last_name").value = "";
+
+  $("first_name").focus();
+};
+
+var clearList = function () {
+  // delete the data from the arrays
+
+  volunteerArray = [];
+
+  // alternative way to delete all of the data from the array
+
+  // volunteerArray.length = 0;
+
+  // remove the volunteers data from the web page
+
+  $("volunteerList").value = "";
+
+  $("first_name").focus();
+};
+
+var sortList = function () {
+  // sort the scores
+
+  volunteerArray.sort();
+
+  // display the scores
+
+  displayVolunteers();
+};
+
+//When the page is fully loaded, the buttons will be mapped to the JavaScript functions
+
+window.onload = function () {
+  $("add_button").onclick = addVolunteer;
+
+  $("delete_button").onclick = deleteVolunteer;
+
+  $("clear_button").onclick = clearList;
+
+  $("sort_button").onclick = sortList;
+
+  $("sort_lbutton").onclick = sortListLastName;
+
+  $("first_name").focus();
+};
+
+//Comparing the last names and sorting based on it
+
+function compare(a, b) {
+  //This function taking first name and the last name
+
+  //Splitting the single string into two string by detecting th space between them
+
+  var splitA = a.split(" ");
+
+  var splitB = b.split(" ");
+
+  var lastNameA = splitA[splitA.length - 1];
+
+  var lastNameB = splitB[splitB.length - 1];
+
+  if (lastNameA < lastNameB) return -1;
+
+  if (lastNameA > lastNameB) return 1;
+
   return 0;
 }
-var sortListFirst_Name = function() {
-  volunteerListArray.sort(compare);
-displayVolunteerList();
-};
+
+var sortListLastName = function () {
+  volunteerArray.sort(compare);
+
+  // display the scores
+
+  displayVolunteers();
 };
